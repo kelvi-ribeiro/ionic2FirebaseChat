11aed8fc025b230acbde6ac7aac2ad737a75f565
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-02-11 13:31:46 -0200

    Aula 75. Aprendendo os variados tipos de loading e mensagens nativos do angular

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -3,7 +3,7 @@ import { AuthProvider } from './../../providers/auth/auth.provider';
 import { UserProvider } from './../../providers/user/user.provider';
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Loading, LoadingController, AlertController } from 'ionic-angular';
 import { User } from '../../models/user.model';
 
 /**
@@ -20,40 +20,67 @@ import { User } from '../../models/user.model';
 })
 export class SignupPage {
 
-  signupForm:FormGroup
+  signupForm: FormGroup
 
   constructor(
-    public authProvider:AuthProvider,
-    public formBuilder:FormBuilder,
+    public alertCtrl: AlertController,
+    public authProvider: AuthProvider,
+    public formBuilder: FormBuilder,
+    public loadingCtrl: LoadingController,
     public navCtrl: NavController,
     public navParams: NavParams,
-    public userProvider:UserProvider) {
-      let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-      this.signupForm = this.formBuilder.group({
-        name:['',[Validators.required,Validators.minLength(3)]],
-        username:['',[Validators.required,Validators.minLength(3)]],
-        email:['',Validators.compose([Validators.required , Validators.pattern(emailRegex)])],
-        password:['',[Validators.required,Validators.minLength(6)]],
-      });
+    public userProvider: UserProvider) {
+    let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    this.signupForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
-  onSubmit():void{
+  onSubmit(): void {
+    let loading: Loading = this.showLoading();
     let formUser = this.signupForm.value;
     this.authProvider.createAuthUser({
-      email:formUser.email,
-      password:formUser.password
+      email: formUser.email,
+      password: formUser.password
     })
-    .then((authState:FirebaseAuthState)=>{
+      .then((authState: FirebaseAuthState) => {
 
-      delete formUser.password;
-      formUser.uid = authState.auth.uid;
-      this.userProvider.createUser(formUser)
-      .then(()=>{
-        console.log('Usuário Cadastrado');
+        delete formUser.password;
+        formUser.uid = authState.auth.uid;
+        this.userProvider.createUser(formUser)
+          .then(() => {
+            console.log('Usuário Cadastrado');
+            loading.dismiss()
+          }).catch((error: Error) => {
+            console.log(error);
+            loading.dismiss();
+            this.showAlert(error.message)
+
+          })
+      }).catch((error: Error) => {
+        console.log(error);
+        loading.dismiss();
+        this.showAlert(error.message)
 
       })
-    })
 
+  }
+  private showLoading(): Loading {
+    let loading: Loading = this.loadingCtrl.create(({
+      content: 'Please wait...'
+    }));
+    loading.present();
+    return loading;
+  }
+
+  private showAlert(message: string): void {
+    this.alertCtrl.create({
+      message: message,
+      buttons: ['OK']
+    }).present();
   }
 
 }
