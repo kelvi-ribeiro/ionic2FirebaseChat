f903e5ddaf398a841d62e2e57c9d860d23e55651
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-02-17 23:13:20 -0300

    Aula 85. Preparando a HomePage com IonSegment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,6 +14,7 @@ import { User } from '../../models/user.model';
 export class HomePage {
 
   users:FirebaseListObservable<User[]>
+  view:string = 'chats';
 
   constructor(
               public authProvider:AuthProvider,
