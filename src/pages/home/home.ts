8672b017e81df6842fd0b7540f5a2f1d7c1a79d1
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-02-21 18:12:38 -0300

    Aula 99. Criando novos chats no firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { ChatProvider } from './../../providers/chat/chat.provider';
 import { AuthProvider } from './../../providers/auth/auth.provider';
 import { FirebaseListObservable } from 'angularfire2';
 import { Component } from '@angular/core';
@@ -7,7 +8,8 @@ import { SignupPage } from './../signup/signup';
 import { UserProvider } from './../../providers/user/user.provider';
 import { User } from '../../models/user.model';
 import { ChatPage } from '../chat/chat';
-
+import { Chat } from '../../models/chat.model';
+import firebase from 'firebase'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -18,6 +20,7 @@ export class HomePage {
   view:string = 'chats';
 
   constructor(
+              public chatProvider:ChatProvider,
               public authProvider:AuthProvider,
               public navCtrl: NavController,
               public userProvider:UserProvider) {
@@ -31,9 +34,27 @@ export class HomePage {
 
 
   }
-  onChatCreate(user:User){
+  onChatCreate(recipenteUser:User){
+    this.userProvider.currentUser
+    .first()
+    .subscribe((currentUser:User)=>{
+      this.chatProvider.getDeepChat(currentUser.$key,recipenteUser.$key)
+      .first()
+      .subscribe((chat:Chat)=>{
+        if(chat.hasOwnProperty('$value')){
+          let timestamp:Object = firebase.database.ServerValue.TIMESTAMP;
+          let chat1 = new Chat('',timestamp,recipenteUser.name,'');
+          this.chatProvider.create(chat1,currentUser.$key,recipenteUser.$key);
+          let chat2 = new Chat('',timestamp,currentUser.name,'');
+          this.chatProvider.create(chat2,recipenteUser.$key,currentUser.$key);
+        }
+
+      });
+
+    })
+
     this.navCtrl.push(ChatPage,{
-      recipenteUser:user
+      recipenteUser:recipenteUser
     })
 
   }
